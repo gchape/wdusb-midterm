ca02420f06294858baf51b117f397c2335c7740a
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-02 14:44:57 +0400

    feat: implement BookRepository and AuthorRepository with derived queries and JPQL search

--- src/main/kotlin/tech/provokedynamic/wdusbmidterm/repository/AuthorRepository.kt
+++ src/main/kotlin/tech/provokedynamic/wdusbmidterm/repository/AuthorRepository.kt
@@ -1,4 +1,8 @@
 package tech.provokedynamic.wdusbmidterm.repository
 
-class AuthorRepository {
+import org.springframework.data.jpa.repository.JpaRepository
+import tech.provokedynamic.wdusbmidterm.entity.Author
+
+interface AuthorRepository : JpaRepository<Author, Int> {
+    fun findAllByDeletedFalse(): List<Author>
 }
--- src/main/kotlin/tech/provokedynamic/wdusbmidterm/repository/BookRepository.kt
+++ src/main/kotlin/tech/provokedynamic/wdusbmidterm/repository/BookRepository.kt
@@ -1,4 +1,28 @@
 package tech.provokedynamic.wdusbmidterm.repository
 
-interface BookRepository {
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import tech.provokedynamic.wdusbmidterm.entity.Book
+
+interface BookRepository : JpaRepository<Book, Int> {
+    fun findByIsbnAndDeletedFalse(isbn: String): Book?
+
+    fun findByDeletedFalseOrderByCreatedAtDesc(pageable: Pageable): List<Book>
+
+    fun findAllByDeletedFalse(pageable: Pageable): Page<Book>
+
+    fun findByGenreIgnoreCaseAndDeletedFalse(genre: String, pageable: Pageable): Page<Book>
+
+    @Query(
+        """
+            SELECT b FROM Book b 
+            WHERE b.deleted = false
+                AND (LOWER(b.title) LIKE LOWER(CONCAT('%', :query, '%')) 
+                OR LOWER(b.isbn) LIKE LOWER(CONCAT('%', :query, '%')) 
+                OR LOWER(b.genre) LIKE LOWER(CONCAT('%', :query, '%')))
+         """
+    )
+    fun search(query: String, pageable: Pageable): Page<Book>
 }
