c81c121b7439507a46fa5e2ce9b1f61fff5e5c93
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-05-06 00:01:04 +0400

    refactor: replace @EnableAutoConfiguration with @ImportAutoConfiguration allowlist

--- src/main/kotlin/tech/provokedynamic/wdusbmidterm/WdusbMidtermApplication.kt
+++ src/main/kotlin/tech/provokedynamic/wdusbmidterm/WdusbMidtermApplication.kt
@@ -1,13 +1,33 @@
 package tech.provokedynamic.wdusbmidterm
 
 import org.springframework.boot.SpringBootConfiguration
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
-import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration
-import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration
-import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration
-import org.springframework.boot.jdbc.autoconfigure.JdbcTemplateAutoConfiguration
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration
+import org.springframework.boot.autoconfigure.context.ConfigurationPropertiesAutoConfiguration
+import org.springframework.boot.autoconfigure.context.LifecycleAutoConfiguration
+import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration
+import org.springframework.boot.cache.autoconfigure.CacheAutoConfiguration
+import org.springframework.boot.data.jpa.autoconfigure.DataJpaRepositoriesAutoConfiguration
+import org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration
+import org.springframework.boot.http.converter.autoconfigure.HttpMessageConvertersAutoConfiguration
+import org.springframework.boot.jackson.autoconfigure.JacksonAutoConfiguration
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration
+import org.springframework.boot.jdbc.autoconfigure.DataSourcePoolMetadataProvidersConfiguration
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration
 import org.springframework.boot.persistence.autoconfigure.EntityScan
+import org.springframework.boot.persistence.autoconfigure.PersistenceExceptionTranslationAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.boot.servlet.autoconfigure.HttpEncodingAutoConfiguration
+import org.springframework.boot.servlet.autoconfigure.MultipartAutoConfiguration
+import org.springframework.boot.tomcat.autoconfigure.servlet.TomcatServletWebServerAutoConfiguration
+import org.springframework.boot.transaction.autoconfigure.TransactionAutoConfiguration
+import org.springframework.boot.transaction.autoconfigure.TransactionManagerCustomizationAutoConfiguration
+import org.springframework.boot.validation.autoconfigure.ValidationAutoConfiguration
+import org.springframework.boot.webmvc.autoconfigure.DispatcherServletAutoConfiguration
+import org.springframework.boot.webmvc.autoconfigure.WebMvcAutoConfiguration
+import org.springframework.boot.webmvc.autoconfigure.error.ErrorMvcAutoConfiguration
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.AdviceMode
 import org.springframework.context.annotation.ComponentScan
@@ -17,14 +37,34 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import java.util.concurrent.TimeUnit
 
 @SpringBootConfiguration(proxyBeanMethods = false)
-@EnableAutoConfiguration(
-    exclude = [
-        JmxAutoConfiguration::class,
-        TaskSchedulingAutoConfiguration::class,
-        TaskExecutionAutoConfiguration::class,
-        JdbcTemplateAutoConfiguration::class
+@ImportAutoConfiguration(
+    classes = [
+        AopAutoConfiguration::class,
+        DataSourceAutoConfiguration::class,
+        DataSourceTransactionManagerAutoConfiguration::class,
+        DataSourcePoolMetadataProvidersConfiguration::class,
+        HibernateJpaAutoConfiguration::class,
+        DataJpaRepositoriesAutoConfiguration::class,
+        PersistenceExceptionTranslationAutoConfiguration::class,
+        CacheAutoConfiguration::class,
+        FlywayAutoConfiguration::class,
+        WebMvcAutoConfiguration::class,
+        DispatcherServletAutoConfiguration::class,
+        TomcatServletWebServerAutoConfiguration::class,
+        HttpMessageConvertersAutoConfiguration::class,
+        HttpEncodingAutoConfiguration::class,
+        MultipartAutoConfiguration::class,
+        ErrorMvcAutoConfiguration::class,
+        JacksonAutoConfiguration::class,
+        ValidationAutoConfiguration::class,
+        TransactionAutoConfiguration::class,
+        TransactionManagerCustomizationAutoConfiguration::class,
+        PropertyPlaceholderAutoConfiguration::class,
+        ConfigurationPropertiesAutoConfiguration::class,
+        LifecycleAutoConfiguration::class,
     ]
 )
+@AutoConfigurationPackage(basePackages = ["tech.provokedynamic.wdusbmidterm"])
 @EnableCaching(mode = AdviceMode.PROXY)
 @ComponentScan(
     basePackages = [
@@ -44,5 +84,5 @@ class WdusbMidtermApplication : WebMvcConfigurer {
 }
 
 fun main(args: Array<String>) {
-    runApplication<WdusbMidtermApplication>(*args)
+    val ctx = runApplication<WdusbMidtermApplication>(*args)
 }
