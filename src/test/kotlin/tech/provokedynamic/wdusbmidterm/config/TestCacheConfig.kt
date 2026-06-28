75d37ba1b124c3e0c55c84d76ab1a51ea2001774
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-06-28 13:31:42 +0400

    test: add TestCacheConfig with ConcurrentMapCacheManager for test isolation

--- src/test/kotlin/tech/provokedynamic/wdusbmidterm/config/TestCacheConfig.kt
+++ src/test/kotlin/tech/provokedynamic/wdusbmidterm/config/TestCacheConfig.kt
@@ -1,4 +1,18 @@
 package tech.provokedynamic.wdusbmidterm.config
 
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.cache.CacheManager
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
+
+@TestConfiguration
+@Profile("dev")
 class TestCacheConfig {
+    @Bean
+    fun cacheManager(): CacheManager = ConcurrentMapCacheManager(
+        "authors:all", "authors:paged", "authors:single", "authors:exists", "authors:count",
+        "genres:all", "genres:single", "genres:exists", "genres:count",
+        "books:all", "books:paged", "books:single", "books:exists", "books:count"
+    )
 }
