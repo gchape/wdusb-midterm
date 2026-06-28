8452b45cb8593b3db89ba9b09e1c33f1a5f4181f
Giorgi Chapidze <chapidze.giorgi@proton.me>
2026-06-28 13:31:42 +0400

    test: fix application context smoke test

--- src/test/kotlin/tech/provokedynamic/wdusbmidterm/WdusbMidtermApplicationTests.kt
+++ src/test/kotlin/tech/provokedynamic/wdusbmidterm/WdusbMidtermApplicationTests.kt
@@ -1,13 +1,27 @@
 package tech.provokedynamic.wdusbmidterm
 
+import com.github.benmanes.caffeine.cache.Caffeine
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cache.CacheManager
+import org.springframework.cache.caffeine.CaffeineCacheManager
+import org.springframework.context.annotation.Bean
+import tech.provokedynamic.wdusbmidterm.config.AppProperties
+import java.util.concurrent.TimeUnit
 
 @SpringBootTest
 class WdusbMidtermApplicationTests {
 
-    @Test
-    fun contextLoads() {
+    @Bean
+    fun cacheManager(appProperties: AppProperties): CacheManager {
+        val manager = CaffeineCacheManager(
+            "authors:exists", "authors:paged", "authors:all", "authors:single", "authors:count"
+        )
+        manager.setCaffeine(
+            Caffeine.newBuilder()
+                .expireAfterWrite(appProperties.maintenance.cacheTtlMinutes.toLong(), TimeUnit.MINUTES)
+                .maximumSize(1000)
+        )
+        return manager
     }
-
 }
